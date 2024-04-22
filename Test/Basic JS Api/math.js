d9d373704f1a6cc02b3db10c02e28425da0c8bf4
Vinal Pawar <vinalpawar1@gmail.com>
2024-04-22 21:53:21 +0530

    revise api's

---
+++ Test/Basic JS Api/math.js
@@ -0,0 +1,13 @@
+console.log("Random: ", Math.random()); // generate random numbers
+
+console.log("Ceil: ", Math.ceil(2.3)); // This method rounds a number upwards to the nearest integer. It returns the smallest integer greater than or equal to a given number.
+
+console.log("Floor: ", Math.floor(2.3)); // This method rounds a number downwards to the nearest integer. It returns the largest integer less than or equal to a given number.
+
+console.log("Max: ", Math.max(5, 10, 15)); // Gives maximum number
+
+console.log("Min: ", Math.min(5, 10, 15)); // Gives minimum number
+
+console.log("Power: ", Math.pow(4, 2)); // This method is used to raise a number to a specified power. It takes two arguments: the base number and the exponent. It returns the result of raising the base to the power of the exponent
+
+console.log("Square root: ", Math.sqrt(16)); // This method is used to find the square root of a number. It takes one argument: the number whose square root is to be found. It returns the square root of the given number.
